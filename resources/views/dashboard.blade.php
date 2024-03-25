2071d77c700f8119fd0788949f52e7f072d7a23b
alenluis <daxay8808@gmail.com>
2024-03-25 18:41:53 +0100

    quinto commit de minisemblantes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+        /* Agrega una regla de estilo para utilizar la fuente Allegrian */
+        @import url('https://fonts.googleapis.com/css2?family=Allegrian&display=swap');
+        
+        .container {
+            font-family: 'Allegrian', cursive; /* Aplica la fuente Allegrian al texto dentro del contenedor */
+        }
+    </style>
+
     <div class="container">
-        <h1 class="">Área de trabajo</h1>
+        <h1 class="text-center">Área de Trabajo</h1>
     </div>
 @endsection
